dd7c51fb3d80112840fc4fea51150fb9c3304cda
Deepte01 <48492289+Deepte01@users.noreply.github.com>
2019-04-09 22:04:03 -0600

    Added Python code for recording the video

--- Python Code/ControlOperation.py
+++ Python Code/ControlOperation.py
@@ -1,6 +1,9 @@
 import serial
 import time
 import pyautogui
+import cv2
+import numpy as np
+
 
 ArduinoSerial=serial.Serial('com3',9600)
 time.sleep(2)
@@ -25,16 +28,30 @@ while 1:
         pyautogui.scroll(100)
 
     if "pagedown" in incoming:
-        pyautogui.hotkey(-100)
+        pyautogui.scroll(-100)
   
     if "Decrease" in incoming:
         pyautogui.hotkey('ctrl','down')
 
-    if 'change' in incoming_data:                  # if incoming data is 'change'
+    if 'change' in incoming:                  # if incoming data is 'change'
         pyautogui.keyDown('alt')                   # performs "alt+tab" operation which switches the tab
         pyautogui.press('tab')
         pyautogui.keyUp('alt')
-    
+        
+    if 'RecordVideo' in incoming:
+        cap=cv2.VideoCapture(0)
+        fourcc=cv2.VideoWriter_fourcc(*'XVID')
+        out=cv2.VideoWriter('output.avi',fourcc,20.0,(640,480))
+        while True:
+            ret, frame=cap.read()
+            gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+            out.write(frame)
+            cv2.imshow('frame',frame)
+            if cv2.waitKey(1) & 0xFF==ord('q'):
+                break
+        cap.release()
+        out.release()
+        cv2.destroyAllWindows()
   
     incoming =""
    
